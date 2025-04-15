88f881b0c1b34a9d55c51c04d496a187595ea413
claucuru <claudia.cuevasru@gmail.com>
2025-04-15 13:14:47 +0200

    ficheros iniciales

---
+++ P5/src/p5/Main.java
@@ -0,0 +1,27 @@
+package p5;
+public class Main {
+	public static void main(String[] args) {
+		StateGraph<NumericData> sg = buildWorflow();
+		
+		System.out.println(sg);
+		
+		NumericData input = new NumericData(2, 3);
+		System.out.println("input = " + input);
+		NumericData output = sg.run(input, true); //ejecución con debug
+		System.out.println("result = " + output);
+		      
+	}
+	
+	private static StateGraph<NumericData> buildWorflow(){
+		StateGraph<NumericData> sg = new StateGraph<>("math2", "Add two numbers, and then square");
+		sg.addNode("sum", (NumericData mo) -> mo.put("result", mo.get("op1")+mo.get("op2")))
+		.addNode("square", (NumericData mo) -> mo.put("result", mo.get("result")*mo.get("result")));
+		
+		sg.addEdge("sum", "square");
+		
+		sg.setInitial("sum");
+		sg.setFinal("square");
+		
+		return sg; 
+	}
+}
---
+++ P5/src/p5/StateGraph.java
@@ -0,0 +1,37 @@
+package p5;
+
+import java.util.function.Consumer;
+
+public class StateGraph<T> {
+	
+	public StateGraph(String s1, String s2) {
+		
+	}
+	
+	public StateGraph<T> addNode(String string, Consumer<T> object) {
+		// TODO Auto-generated method stub
+		
+		return this;
+	}
+
+	public void setInitial(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setFinal(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addEdge(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public NumericData run(NumericData input, boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
