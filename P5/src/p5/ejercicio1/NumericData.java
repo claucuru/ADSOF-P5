b2a0a691a4fbf2f052ef706e01b6e83591cb02d4
claucuru <150439472+claucuru@users.noreply.github.com>
2025-05-08 16:39:11 +0200

    Update NumericData.java

--- P5/src/p5/ejercicio1/NumericData.java
+++ P5/src/p5/ejercicio1/NumericData.java
@@ -1,7 +1,18 @@
 package p5.ejercicio1;
 import java.util.*;
 
+/**
+ * @author Claudia Cuevas Ruano
+ * @author Lucia Espinosa Murillo
+ * Clase que define un dato de tipo numérico
+ */
 public class NumericData extends Data<Integer>{
+
+	/**
+	 * Constructor de la clase
+	 * @param op1 un entero
+	 * @param op2 un entero
+	*/
 	public NumericData(int op1, int op2) {
 		super(op1, op2);
 		super.put("result", 0);
