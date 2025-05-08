f74d534876be4f8bc17093fcdae138228d14f9de
claucuru <150439472+claucuru@users.noreply.github.com>
2025-05-08 16:42:48 +0200

    Update Graph.java

--- P5/src/p5/ejercicio5/Graph.java
+++ P5/src/p5/ejercicio5/Graph.java
@@ -10,10 +10,15 @@ import p5.ejercicio1.StateGraph;
 
 public interface Graph<T extends Data<?>> {
 	
+	/**
+	 * Funcion que añade un  nodo a un grafo
+	 * @param name nombre del nodo
+	 * @param accion del nodo
+	 */
 	Graph<T> addNode(String name, Consumer<T> action);
 	
 	Graph<T> setInitial(String name);
-	
+
 	Node<T> getInitialNode();
 	
 	Node<T> getFinalNode();
@@ -22,10 +27,26 @@ public interface Graph<T extends Data<?>> {
 	
 	List<Node<T>> getNodes();
 
+	/**
+	 * Funcion que relaciona dos nodos en un edge con una condicion. Para ello se añade a una lista del primer nodo
+	 * @param name_node1 
+	 * @param name_node2
+	 * @param action 
+	 * @return el StateGraph
+	 */
 	Graph<T> addConditionalEdge(String node_name1, String node_name2, Predicate<T> action);
-	
+
+	/**
+	 * Funcion que devuelve el historial de un grafo
+	 * @return buffer con el historial
+	 */
 	String history();
-	
+
+	/**
+	 * Funcion que ejecuta todos los nodos de un grafo
+	 * @param input grafo
+	 * @param debug
+	 */
 	T run(T input, boolean debug);
 	
 	String toString();
