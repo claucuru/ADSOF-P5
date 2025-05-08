549a269ecb376e9585392ac63732ab352b72735b
claucuru <150439472+claucuru@users.noreply.github.com>
2025-05-08 16:37:17 +0200

    Update Data.java

--- P5/src/p5/ejercicio1/Data.java
+++ P5/src/p5/ejercicio1/Data.java
@@ -1,7 +1,18 @@
 package p5.ejercicio1;
 import java.util.*;
-
+/**
+ * @author Claudia Cuevas Ruano
+ * @author Lucia Espinosa Murillo
+ * 
+ * Clase que extiende de LinkedHashMap
+ * Guarda operadores
+ */
 public class Data<T> extends LinkedHashMap<String, T>{
+	/**
+	 * Constructor de la clase
+	 * @param T op1
+	 * @param T op2
+	 */
 	public Data(T op1, T op2) {
 		this.put("op1", op1);
 		this.put("op2", op2);
